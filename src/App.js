dd30ee95c32bcd2c5ff38ebdf43e46952ac09eb1
Broady <broadybmx@gmail.com>
2024-12-01 11:30:46 -0500

    feat: Finish posts page and functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Posts from "./pages/Posts.jsx";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path=":id"></Route>
+          <Route path=":id" element={<Posts />}></Route>
         </Routes>
       </div>
     </Router>
